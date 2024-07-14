2173bd3ce86cf730c944bb32912e5d02b362535c
himanshu3232 <himanshu2000ranjan@gmail.com>
2024-07-14 23:12:30 +0530

    Initial commit

---
+++ DevCart/src/components/Home.tsx
@@ -0,0 +1,8 @@
+import Header from "./header/Header"
+export default function Home() {
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
---
+++ DevCart/src/components/header/Header.tsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <>
+      <div className="headerWrapper">
+        <div className="top-strip bg-blue">
+          <div className="container">
+            <p className="mb-0 mt-0 text-center bg-purple color-white">
+              <b>Free shipping</b> on orders above ₹999
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
