b30e9c638ed4e6ff2a3d08bce4dab0431285fb60
himanshu3232 <himanshu2000ranjan@gmail.com>
2024-07-24 14:29:38 +0530

    Minor changes

--- DevCart/src/components/header/LogoRow.tsx
+++ DevCart/src/components/header/LogoRow.tsx
@@ -12,7 +12,7 @@ export default function Logo() {
           <div className="row">
             <div className="logoWrapper d-flex align-items-center col-sm-2">
               <Link to={"/"}>
-                <img src={img} alt="logo" />
+                <img className="logo" src={img} alt="logo" />
               </Link>
             </div>
             <div className="col-sm-10 d-flex align-items-center part2">
