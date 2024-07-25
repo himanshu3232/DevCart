0a7169fe9020d0c4eb9fede3b9c095a4deee027e
himanshu3232 <himanshu2000ranjan@gmail.com>
2024-07-25 16:37:26 +0530

    Added col nav

--- DevCart/src/components/nav/CategoriesCol.tsx
+++ DevCart/src/components/nav/CategoriesCol.tsx
@@ -1,30 +1,51 @@
 import { Link } from "react-router-dom";
+import { CiHome } from "react-icons/ci";
+import { GiClothes } from "react-icons/gi";
+import { MdPhonelink } from "react-icons/md";
+import { MdOutlineEmojiFoodBeverage } from "react-icons/md";
+import { GiFruitBowl } from "react-icons/gi";
+import { TfiWrite } from "react-icons/tfi";
+import { CiHeadphones } from "react-icons/ci";
 
 export default function CategoriesCol() {
   return (
     <>
       <div className="col-sm-9 nav2 d-flex align-items-center">
-        <ul style={{marginLeft: 'auto'}} className="list list-inline">
+        <ul style={{ marginLeft: "auto" }} className="list list-inline">
           <li className="list-inline-item">
-            <Link to={'/'}>Home</Link>
+            <Link to={"/"}>
+              <CiHome /> &nbsp; Home
+            </Link>
           </li>
           <li className="list-inline-item">
-            <Link to={'/fashion'}>Fashion</Link>
+            <Link to={"/fashion"}>
+              <GiClothes /> &nbsp;Fashion
+            </Link>
           </li>
           <li className="list-inline-item">
-            <Link to={'/electronics'}>Electronics</Link>
+            <Link to={"/electronics"}>
+              <MdPhonelink /> &nbsp;Electronics
+            </Link>
           </li>
           <li className="list-inline-item">
-            <Link to={'/bakery'}>Bakery</Link>
+            <Link to={"/bakery"}>
+              <MdOutlineEmojiFoodBeverage /> &nbsp;Bakery
+            </Link>
           </li>
           <li className="list-inline-item">
-            <Link to={'/groceries'}>Groceries</Link>
+            <Link to={"/groceries"}>
+              <GiFruitBowl /> &nbsp;Groceries
+            </Link>
           </li>
           <li className="list-inline-item">
-            <Link to={'/blog'}>Blog</Link>
+            <Link to={"/blog"}>
+              <TfiWrite /> &nbsp;Blog
+            </Link>
           </li>
           <li className="list-inline-item">
-            <Link to={'/contact-us'}>Contact Us</Link>
+            <Link to={"/contact-us"}>
+              <CiHeadphones /> &nbsp;Contact Us
+            </Link>
           </li>
         </ul>
       </div>
