c6c405e5c61c7d31bc538d822bd6ee1757b17d26
himanshu3232 <himanshu2000ranjan@gmail.com>
2024-08-06 20:16:46 +0530

    Added post service

--- DevCart/src/components/header/CountryDropdown.tsx
+++ DevCart/src/components/header/CountryDropdown.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@mui/material";
 import { FaAngleDown } from "react-icons/fa";
+import Dialog from "@mui/material/Dialog";
+import { IoIosSearch } from "react-icons/io";
 
 export default function CountryDropdown() {
   return (
@@ -9,10 +11,22 @@ export default function CountryDropdown() {
           <span className="label">Your Location</span>
           <span className="name">India</span>
         </div>
-        <span style={{marginLeft: 'auto'}}>
+        <span style={{ marginLeft: "auto" }}>
           <FaAngleDown />
         </span>
       </Button>
+
+      <Dialog open={true} className="location-modal">
+        <h4>Choose your delivery location</h4>
+        <p>Enter your address and we will specify the offer for your area.</p>
+
+        <div className="headerSearch w-75">
+          <input type="text" placeholder="Search for products" />
+          <Button>
+            <IoIosSearch />
+          </Button>
+        </div>
+      </Dialog>
     </>
   );
 }
